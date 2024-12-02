3f83b6fb6136ec0e44869779a3ef9acab1e2cbce
Brandon Barker <barkerbarker1226@gmail.com>
2024-12-02 13:06:16 -0500

    Created a "Hello World" ROM in the .gg file format to be run on any Game Gear emulator

---
+++ romDev/assets.h
@@ -0,0 +1,18 @@
+extern const unsigned char	DemoBG_bin[];
+#define				DemoBG_bin_size 32
+
+extern const unsigned char	DemoBG_psgcompr[];
+#define				DemoBG_psgcompr_size 1172
+
+extern const unsigned char	DemoBG_stmcompr[];
+#define				DemoBG_stmcompr_size 56
+
+extern const unsigned char	HelloWorldPalette_bin[];
+#define				HelloWorldPalette_bin_size 32
+
+extern const unsigned char	HelloWorldTilemap_stmcompr[];
+#define				HelloWorldTilemap_stmcompr_size 156
+
+extern const unsigned char	HelloWorldTiles_psgcompr[];
+#define				HelloWorldTiles_psgcompr_size 1032
+
